9f31807ad834b0e80c2a3023c15acce227bc104f
kituNew <vlad640vlad@gmail.com>
2025-02-12 18:13:48 +0300

    Initial Commit

---
+++ ManagerOfSpending/ManagerOfSpendingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManagerOfSpendingApp.swift
+//  ManagerOfSpending
+//
+//  Created by Zaitsev Vladislav on 12.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ManagerOfSpendingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ManagerOfSpendingTests/ManagerOfSpendingTests.swift
@@ -0,0 +1,17 @@
+//
+//  ManagerOfSpendingTests.swift
+//  ManagerOfSpendingTests
+//
+//  Created by Zaitsev Vladislav on 12.02.2025.
+//
+
+import Testing
+@testable import ManagerOfSpending
+
+struct ManagerOfSpendingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
